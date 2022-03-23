1def8100200f259ecd46df82e4bf95181966787f
kutman00 <kutmankalmurzaev45@gmail.com>
2022-03-23 23:30:25 +0600

    added method

--- src/kg/geektech/game/player/Berserk.java
+++ src/kg/geektech/game/player/Berserk.java
@@ -1,7 +1,6 @@
 package kg.geektech.game.player;
 
 import kg.geektech.game.general.RPG_Game;
-
 public class Berserk extends Hero {
     public Berserk(String name, int health, int damage) {
         super(name, health, damage, SuperAbility.SAVE_DAMAGE_AND_REVERT);
@@ -9,12 +8,16 @@ public class Berserk extends Hero {
 
     @Override
     public void hit(Hero[] heroes) {
+
     }
 
     @Override
     public void applySuperPower(Boss boss, Hero[] heroes) {
-        int coeff = RPG_Game.random.nextInt(20);
-        boss.setHealth(boss.getHealth()-(this.getDamage()+ coeff));
+
+        int coeff = RPG_Game.random.nextInt(40);
+        boss.setHealth(boss.getHealth() - (this.getDamage() + coeff));
+        this.setHealth(this.getHealth()-(boss.getDamage()-coeff));
         System.out.println("Berserk атаковал " + (this.getDamage() + coeff));
+
+        }
     }
-}
